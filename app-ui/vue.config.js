f76cbea8114acf11777fd6475c7973a64a288cab
东风 <1335799468@qq.com>
2023-05-29 18:01:26 +0800

    :bug: The second proxy fixed does not work correctly in the vue.config.js file

--- app-ui/vue.config.js
+++ app-ui/vue.config.js
@@ -1,18 +1,25 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
     devServer: {
-        host: '0.0.0.0',
-        port: 8081,
+        host: 'localhost',
+        port: 8085,
         // open: true,
         //以上的ip和端口是我们本机的;下面为需要跨域的
         proxy: { //配置跨域
-            "/api": {
-                target: 'http://localhost:8080', //这里是调本地的后端,在服务器的时候也是调服务器本地的后端
-                // target: 'http://ip:8080', //这种是在本地调服务器的后端
-                changOrigin: true, //允许跨域
+
+            "/cabin": {
+                //这里是调本地的后端,在服务器的时候也是调服务器本地的后端
+                target: 'http://localhost:8080',
+                //允许跨域
+                changeOrigin: true,
                 ws: false,
-                pathRewrite: {"^/api": ""}
+                pathRewrite: {"^/cabin": ""}
+            },
+            "/chat": {
+                target: 'http://localhost:8081',
+                changeOrigin: false,
+                ws: true,
+                pathRewrite: {"^/chat": ""}
             }
         }
     }
-})
+}
