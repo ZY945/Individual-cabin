c8f09ef27257bd978a9946c3d23737acc24d33b8
东风 <1335799468@qq.com>
2023-06-30 09:33:46 +0800

    test:jenkins + github push

--- app/src/test/java/com/cabin/service/impl/CabinSqlServiceTest.java
+++ app/src/test/java/com/cabin/service/impl/CabinSqlServiceTest.java
@@ -1,58 +1,21 @@
 package com.cabin.service.impl;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
 /**
  * @author 伍六七
  * @date 2023/5/22 11:12
  */
-@SpringBootTest
 public class CabinSqlServiceTest {
-//
-//    @Autowired
-//    StudentMapper studentMapper;
-//    //单条插入
-//    @Test
-//    void testInsertOne(){
-//        Student student1 = new Student();
-//        student1.setSname("ww");
-//        student1.setValue(235.12);
-//        student1.setTime(Instant.ofEpochMilli(1640966500000L));
-//        studentMapper.insertOne(student1);
-//    }
-//
-//    //批量插入
-//    @Test
-//    void testInsertBatch(){
-//        Student student1 = new Student();
-//        student1.setSname("zs");
-//        student1.setValue(123.45);
-//        student1.setTime(Instant.ofEpochMilli(1640966400000L));
-//
-//        Student student2 = new Student();
-//        student2.setSname("ls");
-//        student2.setValue(666.21);
-//        student2.setTime(Instant.ofEpochMilli(1640966300000L));
-//
-//        List<Student> studentList = new ArrayList<>();
-//
-//        studentList.add(student1);
-//        studentList.add(student2);
-//        studentMapper.insertBatch(studentList);
-//    }
-//
-//    //查询
-//    @Test
-//    void testSelect(){
-//        List<Student> studentList = studentMapper.selectByName("zs");
-//        for (Student student : studentList) {
-//            System.out.println(student);
-//        }
-//    }
-//
-//    //删除
+    //删除
 //    @Test
 //    void testDelete(){
-//        studentMapper.deleteAll();
+//        Routing routing = new Routing();
+//        routing.setId(1L);
+//        routing.setDeleted(1);
+//        routing.setPath("sda");
+//        System.out.println(routing.toString());
+//        Routing count = JacksonUtils.convertValue(routing.toString(), Routing.class);
+//        System.out.println(count);
+//        System.out.println(count.toString());
+//
 //    }
 }
