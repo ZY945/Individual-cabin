58926c84b0de8c2dd7b8a980b409a6bbade801eb
东风 <zhangyang_2002@foxmail.com>
2024-05-24 15:11:12 +0800

    Standardized format

--- cabin-app/src/main/java/com/cabin/controller/UtilController.java
+++ cabin-app/src/main/java/com/cabin/controller/UtilController.java
@@ -15,7 +15,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileOutputStream;
 import java.util.List;
--- cabin-oauth2/src/main/java/com/cabin/oauth2/empty/User.java
+++ cabin-oauth2/src/main/java/com/cabin/oauth2/empty/User.java
@@ -1,6 +1,13 @@
 package com.cabin.oauth2.empty;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
